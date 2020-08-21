ccc023eb9e5534ab380cd33bac5c5401e6546260
Chen Xu <windoze@0d0a.com>
2020-08-21 09:16:24 +0800

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,14 @@ struct Token {
 
 impl Token {
     fn is_expired(&self) -> bool {
-        let exp = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(self.expires_on.parse().unwrap(), 0), Utc);
+        let sec = match self.expires_on.parse() {
+            Ok(v) => v,
+            Err(_) => {
+                eprintln!("WARNING: Invalid token expiration value.");
+                return true;
+            }
+        };
+        let exp = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(sec, 0), Utc);
         let duration = exp.signed_duration_since(Utc::now());
         duration.num_minutes() < 5
     }
@@ -62,8 +69,8 @@ impl Profile {
         cache_dir.push("tokengen");
         match create_dir_all(cache_dir.as_path()) {
             Ok(_) => (),
-            Err(_) => {
-                eprintln!("WARNING: Unable to create cache directory.");
+            Err(e) => {
+                eprintln!("WARNING: Unable to create cache directory '{}', error is {:#?}.", cache_dir.to_string_lossy(), e);
                 return HashMap::new();
             }
         }
@@ -78,17 +85,11 @@ impl Profile {
         };
 
         match serde_json::from_reader(cache_file) {
-            Ok(x) => x,
-            Err(_) => {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("WARNING: Unable to load cache file at '{}', error is {:#?}.", cache_filename.to_string_lossy(), e);
                 HashMap::new()
             }
-        }
-    }
-
-    fn find_token(&self, cache: &HashMap<String, Token>) -> Option<Token> {
-        match cache.get(&self.get_key()).cloned() {
-            Some(t) => if t.is_expired() { None } else { Some(t) }
-            None => None
         }
     }
 
@@ -97,8 +98,9 @@ impl Profile {
         cache_dir.push("tokengen");
         match create_dir_all(cache_dir.as_path()) {
             Ok(_) => (),
-            Err(_) => {
-                eprintln!("WARNING: Unable to create cache directory.");
+            Err(e) => {
+                eprintln!("WARNING: Unable to create cache directory '{}', error is {:#?}.", cache_dir.to_string_lossy(), e);
+                return;
             }
         }
 
@@ -106,8 +108,8 @@ impl Profile {
         cache_filename.push("cache.json");
         let cache_file = match File::create(cache_filename.as_path()) {
             Ok(f) => f,
-            Err(_) => {
-                eprintln!("WARNING: Unable to create cache file.");
+            Err(e) => {
+                eprintln!("WARNING: Unable to create cache file at '{}', error is {:#?}.", cache_filename.to_string_lossy(), e);
                 return;
             }
         };
@@ -116,17 +118,24 @@ impl Profile {
             !v.is_expired()
         ).collect();
 
-        serde_json::to_writer(cache_file, &output).unwrap();
+        match serde_json::to_writer(cache_file, &output) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("WARNING: Unable to save cache to '{}', error is {:#?}.", cache_filename.to_string_lossy(), e);
+            }
+        };
     }
 
     fn get_token(&self) -> Token {
         let mut cache = self.load_cache();
 
-        match self.find_token(&cache) {
-            Some(t) => return t,
+        match cache.get(&self.get_key()).filter(|t| !t.is_expired()) {
+            Some(t) => return t.clone(),
             None => ()
         }
 
+        // Refer to:
+        // https://docs.microsoft.com/en-us/azure/active-directory/azuread-dev/v1-oauth2-client-creds-grant-flow
         let url = format!("{}/{}/oauth2/token", self.authority, self.tenant);
 
         let mut form = HashMap::new();
@@ -146,7 +155,14 @@ impl Profile {
             eprintln!("ERROR: Request failed, status is {}", resp.status());
             exit(i32::from(resp.status().as_u16()))
         }
-        let token: Token = resp.json().unwrap();
+
+        let token: Token = match resp.json() {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("ERROR: Failed to decode response, error is {:#?}.", e);
+                exit(2);
+            }
+        };
 
         cache.insert(self.get_key(), token.clone());
 
@@ -176,9 +192,9 @@ impl Configuration {
         config_dir.push("tokengen");
         match create_dir_all(config_dir.as_path()) {
             Ok(_) => (),
-            Err(_) => {
-                eprintln!("WARNING: Unable to create config directory.");
-                return Configuration::new();
+            Err(e) => {
+                eprintln!("WARNING: Unable to create config directory '{}', error is {:#?}.", config_dir.to_string_lossy(), e);
+                return Self::new();
             }
         }
 
@@ -187,11 +203,17 @@ impl Configuration {
         let config_file = match File::open(config_filename.as_path()) {
             Ok(f) => f,
             Err(_) => {
-                return Configuration::new();
+                return Self::new();
             }
         };
 
-        serde_json::from_reader(config_file).unwrap()
+        match serde_json::from_reader(config_file) {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("WARNING: Unable to load configuration file at '{}', error is {:#?}.", config_filename.to_string_lossy(), e);
+                Self::new()
+            }
+        }
     }
 
     fn get_profile(&self,
@@ -221,9 +243,9 @@ fn main() {
     let matches = clap_app!(tokengen =>
         (version: "0.1")
         (author: "Chen Xu <windoze@0d0a.com>")
-        (about: "Generate AzureAD token")
+        (about: "Generate AzureAD token.")
         (@arg PROFILE: -p --profile +takes_value "Profile Name")
-        (@arg CLIENT_ID: -c --client-id +takes_value "AAD Client Id")
+        (@arg CLIENT_ID: -c --client_id +takes_value "AAD Client Id")
         (@arg SECRET: -s --secret +takes_value "Client Secret")
         (@arg TENANT: -t --tenant +takes_value "AAD Tenant")
         (@arg AUTHORITY: -a --authority +takes_value "Authority")
@@ -248,9 +270,9 @@ fn main() {
                                   resource,
     );
     let token = profile.get_token();
-    if format == "header" {
+    if format.starts_with("h") {
         print!("Authorization: Bearer {}", token.access_token);
-    } else if format == "raw" {
+    } else if format.starts_with("r") {
         print!("{}", token.access_token);
     }
 }
